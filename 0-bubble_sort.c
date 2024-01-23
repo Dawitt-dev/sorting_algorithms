f049563b1ee7b3d9ab4a7714afb03f4379708b27
Dawitt-dev <dawittbeyene22@gmail.com>
2024-01-22 18:01:27 -0800

    rewritten

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,22 +11,24 @@ void swap(int *x, int *y);
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	bool swapped;
+	int swapped;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				swapped = true;
-				print_array(array, size);
+				swapped = 1;
 			}
 
 		}
-		if (swapped == false)
+
+		print_array(array, size);
+
+		if (!swapped)
 		{
 			break;
 		}
